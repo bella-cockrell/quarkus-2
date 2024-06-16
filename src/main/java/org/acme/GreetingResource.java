e0f8c32e7119d9def1b9e6ccec419b8b3771d740
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-06-16 21:19:30 +0200

    add greeting as model and persistence

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,17 +1,24 @@
 package org.acme;
 
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.acme.models.Greeting;
 import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
+    @Transactional
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@RestQuery String name) {
+        var greeting = new Greeting();
+        greeting.name = name;
+        greeting.persist();
+
         return "Hello " + name;
     }
 }
