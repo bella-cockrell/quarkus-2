3d75b678b403990b6c2c09f850d92020458e4169
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-06-16 21:14:15 +0200

    add query param option to endpoint

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,13 +4,14 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String hello(@RestQuery String name) {
+        return "Hello " + name;
     }
 }
